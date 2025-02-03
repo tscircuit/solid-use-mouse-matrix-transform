488e64a2741b9e0025b10d54c0f0fdc71598a923
seveibar <seveibar@gmail.com>
2025-02-02 23:39:18 -0500

    wip, fix panning, start work on wheels

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { createEffect, createSignal, onCleanup } from "solid-js"
+import { createEffect, createSignal, createMemo, onCleanup } from "solid-js"
 import solidLogo from "./assets/solid.svg"
 import viteLogo from "/vite.svg"
 import "./App.css"
@@ -7,6 +7,7 @@ import {
   compose,
   identity,
   Matrix,
+  scale,
   translate,
 } from "transformation-matrix"
 import { getMousePosInElm } from "./getMousePosInElm"
@@ -45,28 +46,44 @@ function App() {
       setBaseTransform(compose(baseTransform(), activeModificationTransform()))
       setActiveModificationTransform(identity())
     }
+    const handleWheel = (e: WheelEvent) => {
+      const center = getMousePosInElm(container, e)
+      // const factor = 1 - e.deltaY / 1000
+      // const newTf = compose(
+      //   translate(center.x, center.y),
+      //   scale(factor, factor),
+      //   translate(-center.x, -center.y),
+      //   baseTransform(),
+      // )
+      // setBaseTransform(newTf)
+      e.preventDefault()
+    }
 
     container.addEventListener("mousemove", handleMouseMove)
     container.addEventListener("mousedown", handleMouseDown)
     container.addEventListener("mouseup", handleMouseUp)
+    container.addEventListener("wheel", handleWheel)
 
     onCleanup(() => {
       container.removeEventListener("mousemove", handleMouseMove)
       container.removeEventListener("mousedown", handleMouseDown)
       container.removeEventListener("mouseup", handleMouseUp)
+      container.removeEventListener("wheel", handleWheel)
     })
   })
 
-  const transform = compose(baseTransform(), activeModificationTransform())
+  const transform = createMemo(() =>
+    compose(baseTransform(), activeModificationTransform()),
+  )
 
-  const boxPos = applyToPoint(transform, { x: 0, y: 0 })
-  const scale = transform.a
+  const boxPos = createMemo(() => applyToPoint(transform(), { x: 0, y: 0 }))
+  const scale = createMemo(() => transform().a)
 
   return (
     <div style={{ width: "100vw", height: "100vh" }} ref={setContainerElm}>
       <h1
         style={{
-          transform: `translate(${boxPos.x}px, ${boxPos.y}px) scale(${scale})`,
+          transform: `translate(${boxPos().x}px, ${boxPos().y}px) scale(${scale()})`,
           "transform-origin": "0,0",
         }}
       >
@@ -74,10 +91,10 @@ function App() {
       </h1>
       <div
         style={{
-          left: `${boxPos.x}px`,
-          top: `${boxPos.y}px`,
-          width: `${100 * scale}px`,
-          height: `${100 * scale}px`,
+          left: `${boxPos().x}px`,
+          top: `${boxPos().y}px`,
+          width: `${100 * scale()}px`,
+          height: `${100 * scale()}px`,
           "background-color": "red",
           position: "absolute",
         }}
